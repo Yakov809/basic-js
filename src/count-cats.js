33b449ade7ddcddc212239d02620f8f3225263c3
Yakov809 <chernov809@yandex.ru>
2023-12-18 19:12:03 +0300

    feat:add count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+let matrix = [
+  [0, 1, '^^'],
+  [0, '^^', 2],
+  ['^^', 1, 2],
+  ['^^', 1, '^^']
+];
+function countCats(matrix) {
+  let cats = []; //пустой массив для ловки кошечек
+  for( let i = 0; i < matrix.length; i++ ){
+      for( let j = 0; j < matrix[i].length; j++){
+          // console.log( `item {${i},${j}} - ${matrix[i][j]}`);
+          if (matrix [i][j] === '^^' ){
+              cats.push(j);
+          }
+      }
+  }
+  return cats.length; //Количество кошечек
+  // console.log(cats);
+  // console.log(cats.length);
+
 }
 
 module.exports = {
